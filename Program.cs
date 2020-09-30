7e1288f8c96d391d7be660e0f71bdb645fe1fb9e
Ryan <zelda0169@gmailcom>
2020-09-30 17:02:33 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 5;
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
